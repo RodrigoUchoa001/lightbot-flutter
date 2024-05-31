196bf49dd2a5cacbfd88e34e2aa361fd3b570ccc
RodrigoUchoa001 <franciscorferreira@aluno.uespi.br>
2024-05-31 18:20:14 -0300

    criada tela inicial do jogo

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:lightbot_flutter/screens/game_screen.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          minimum: const EdgeInsets.all(20),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                const Text(
+                  'LightBot',
+                  style: TextStyle(fontSize: 48),
+                ),
+                SizedBox(
+                  height: 48,
+                  child: FilledButton.tonal(
+                    onPressed: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => const GameScreen(),
+                        ),
+                      );
+                    },
+                    child: const Text('Jogar', style: TextStyle(fontSize: 24)),
+                  ),
+                )
+              ],
+            ),
+          )),
+    );
+  }
+}
