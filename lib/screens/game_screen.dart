1836700d99805497ab037ce1c4becb38d5fbfdb1
RodrigoUchoa001 <franciscorferreira@aluno.uespi.br>
2024-06-04 09:43:59 -0300

    adicionada funções de exibir codigo na GameScreen

--- lib/screens/game_screen.dart
+++ lib/screens/game_screen.dart
@@ -1,15 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:lightbot_flutter/models/casa_do_tabuleiro.dart';
 import 'package:lightbot_flutter/models/direcao.dart';
 import 'package:lightbot_flutter/models/orientacao.dart';
 import 'package:lightbot_flutter/providers/game_provider.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_highlight/flutter_highlight.dart';
+import 'package:flutter_highlight/themes/github.dart'; // Escolha um tema de sua preferência
 
 class GameScreen extends StatelessWidget {
   const GameScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    // Forçar orientação paisagem
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeRight,
+      DeviceOrientation.landscapeLeft,
+    ]);
+
     final provider = Provider.of<GameProvider>(context);
 
     print(
@@ -21,66 +30,125 @@ class GameScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Lightbot nível ${provider.nivelAtual + 1}'),
       ),
-      body: Column(
+      body: Row(
         children: [
           Expanded(
-            child: Consumer<GameProvider>(
-              builder: (context, provider, child) {
-                return GridView.builder(
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: provider.tabuleiro.colunas,
-                  ),
-                  itemCount:
-                      provider.tabuleiro.linhas * provider.tabuleiro.colunas,
-                  itemBuilder: (context, index) {
-                    int linha = index ~/ provider.tabuleiro.colunas;
-                    int coluna = index % provider.tabuleiro.colunas;
-                    CasaDoTabuleiro casa =
-                        provider.tabuleiro.casas[linha][coluna];
+            flex: 1,
+            child: Column(
+              children: [
+                Expanded(
+                  child: Consumer<GameProvider>(
+                    builder: (context, provider, child) {
+                      return LayoutBuilder(
+                        builder: (context, constraints) {
+                          double maxHeight = constraints.maxHeight;
+                          double maxWidth = constraints.maxWidth;
+                          int linhas = provider.tabuleiro.linhas;
+                          int colunas = provider.tabuleiro.colunas;
+                          double cellSize =
+                              (maxHeight < maxWidth / colunas * linhas)
+                                  ? maxHeight / linhas
+                                  : maxWidth / colunas;
 
-                    return Container(
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.black, width: 2.0),
-                        color: casa.objetoPresente
-                            ? Colors.grey
-                            : linha ==
-                                        provider
-                                            .tabuleiro.posicaoVitoria.linha &&
-                                    coluna ==
-                                        provider.tabuleiro.posicaoVitoria.coluna
-                                ? Colors.green
-                                : Colors.white,
-                      ),
-                      child: linha == provider.tabuleiro.posicaoRobo!.linha &&
-                              coluna == provider.tabuleiro.posicaoRobo!.coluna
-                          ? Padding(
-                              padding: const EdgeInsets.all(12),
-                              child: Image.asset(
-                                _getRobotImage(
-                                    provider.tabuleiro.orientacaoRobo),
-                                fit: BoxFit.contain,
+                          return SingleChildScrollView(
+                            child: SizedBox(
+                              height: cellSize * linhas,
+                              width: cellSize * colunas,
+                              child: GridView.builder(
+                                physics: NeverScrollableScrollPhysics(),
+                                gridDelegate:
+                                    SliverGridDelegateWithFixedCrossAxisCount(
+                                  crossAxisCount: colunas,
+                                  childAspectRatio: 1,
+                                ),
+                                itemCount: linhas * colunas,
+                                itemBuilder: (context, index) {
+                                  int linha = index ~/ colunas;
+                                  int coluna = index % colunas;
+                                  CasaDoTabuleiro casa =
+                                      provider.tabuleiro.casas[linha][coluna];
+
+                                  return Container(
+                                    decoration: BoxDecoration(
+                                      border: Border.all(
+                                          color: Colors.black, width: 2.0),
+                                      color: casa.objetoPresente
+                                          ? Colors.grey
+                                          : linha ==
+                                                      provider
+                                                          .tabuleiro
+                                                          .posicaoVitoria
+                                                          .linha &&
+                                                  coluna ==
+                                                      provider.tabuleiro
+                                                          .posicaoVitoria.coluna
+                                              ? Colors.green
+                                              : Colors.white,
+                                    ),
+                                    child: linha ==
+                                                provider.tabuleiro.posicaoRobo!
+                                                    .linha &&
+                                            coluna ==
+                                                provider.tabuleiro.posicaoRobo!
+                                                    .coluna
+                                        ? Padding(
+                                            padding: const EdgeInsets.all(12),
+                                            child: Image.asset(
+                                              _getRobotImage(provider
+                                                  .tabuleiro.orientacaoRobo),
+                                              fit: BoxFit.contain,
+                                            ),
+                                          )
+                                        : null,
+                                  );
+                                },
                               ),
-                            )
-                          : null,
-                    );
-                  },
-                );
-              },
+                            ),
+                          );
+                        },
+                      );
+                    },
+                  ),
+                ),
+              ],
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Consumer<GameProvider>(
-              builder: (context, provider, child) {
-                return Column(
-                  children: [
-                    const Text(
-                      'Comandos Selecionados:',
-                      style:
-                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+          Expanded(
+            flex: 1,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Código gerado:',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                  Expanded(
+                    child: Container(
+                      padding: const EdgeInsets.all(8.0),
+                      child: SingleChildScrollView(
+                        child: HighlightView(
+                          _gerarCodigo(provider.comandos),
+                          language: 'dart', // ou a linguagem que você desejar
+                          theme: githubTheme, // ou outro tema que você preferir
+                          padding: const EdgeInsets.all(12),
+                          textStyle: const TextStyle(
+                            fontFamily: 'Courier',
+                            fontSize: 14,
+                          ),
+                        ),
+                      ),
                     ),
-                    Wrap(
-                      spacing: 10.0,
+                  ),
+                  const SizedBox(height: 12),
+                  const Text(
+                    'Comandos Selecionados:',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
                       children: provider.comandos.map((comando) {
                         if (comando == Direcao.virarEsquerda) {
                           return const Chip(
@@ -91,67 +159,77 @@ class GameScreen extends StatelessWidget {
                               label: Icon(Icons.rotate_right_rounded));
                         }
                         return const Chip(label: Icon(Icons.straight_rounded));
-                        // return Chip(
-                        //   label: Text(comando.toString().split('.').last),
-                        // );
                       }).toList(),
                     ),
-                  ],
-                );
-              },
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                SizedBox(
-                  height: 50,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Provider.of<GameProvider>(context, listen: false)
-                          .adicionarComando(Direcao.virarEsquerda);
-                    },
-                    child: const Icon(Icons.rotate_left_rounded, size: 32),
                   ),
-                ),
-                SizedBox(
-                  height: 50,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Provider.of<GameProvider>(context, listen: false)
-                          .adicionarComando(Direcao.avancar);
-                    },
-                    child: const Icon(Icons.straight_rounded, size: 32),
-                  ),
-                ),
-                SizedBox(
-                  height: 50,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Provider.of<GameProvider>(context, listen: false)
-                          .adicionarComando(Direcao.virarDireita);
-                    },
-                    child: const Icon(Icons.rotate_right_rounded, size: 32),
-                  ),
-                ),
-                SizedBox(
-                  height: 50,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Provider.of<GameProvider>(context, listen: false)
-                          .executarComandos(context);
-                    },
-                    child: const Text('Executar'),
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ],
       ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            SizedBox(
+              height: 50,
+              child: ElevatedButton(
+                onPressed: () {
+                  Provider.of<GameProvider>(context, listen: false)
+                      .adicionarComando(Direcao.virarEsquerda);
+                },
+                child: const Icon(Icons.rotate_left_rounded, size: 32),
+              ),
+            ),
+            SizedBox(
+              height: 50,
+              child: ElevatedButton(
+                onPressed: () {
+                  Provider.of<GameProvider>(context, listen: false)
+                      .adicionarComando(Direcao.avancar);
+                },
+                child: const Icon(Icons.straight_rounded, size: 32),
+              ),
+            ),
+            SizedBox(
+              height: 50,
+              child: ElevatedButton(
+                onPressed: () {
+                  Provider.of<GameProvider>(context, listen: false)
+                      .adicionarComando(Direcao.virarDireita);
+                },
+                child: const Icon(Icons.rotate_right_rounded, size: 32),
+              ),
+            ),
+            SizedBox(
+              height: 50,
+              child: ElevatedButton(
+                onPressed: () {
+                  Provider.of<GameProvider>(context, listen: false)
+                      .executarComandos(context);
+                },
+                child: const Text('Executar'),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
+  }
+
+  String _gerarCodigo(List<Direcao> comandos) {
+    return comandos.map((comando) {
+      if (comando == Direcao.virarEsquerda) {
+        return 'robo.virarEsquerda();';
+      } else if (comando == Direcao.virarDireita) {
+        return 'robo.virarDireita();';
+      } else if (comando == Direcao.avancar) {
+        return 'robo.irEmFrente();';
+      }
+      return '';
+    }).join('\n');
   }
 
   String _getRobotImage(Orientacao orientacao) {
